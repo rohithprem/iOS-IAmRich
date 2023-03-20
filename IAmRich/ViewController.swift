6d6d7dfe0a7c662ad4f21c5930e00382b964624a
rohithprem <rohithp@live.com>
2023-03-20 10:14:37 +0100

    Initial Commit

---
+++ IAmRich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IAmRich
+//
+//  Created by Rohith Premachandran on 19.03.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
